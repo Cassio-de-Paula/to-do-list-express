30114bdbcb0cb53f0b3a9124af2fe161d9afda6c
Cassio Renan <cassio.rr.de.paula@gmail.com>
2023-05-15 12:21:11 -0300

    problemas com update de tarefas

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
 mongoose
-  .connect("mongodb://127.0.0.1/to-do-list", {
+  .connect("mongodb://127.0.0.1:27017/to-do-list", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
